fc625d74db4d00ca4eee7b7fcceb660202683a14
Ivan Pekov <ivan@mrivanplays.com>
2022-03-11 21:16:56 +0200

    introduce SerializationContext

---
+++ core/src/main/java/com/mrivanplays/annotationconfig/core/serialization/SerializationContext.java
@@ -0,0 +1,24 @@
+package com.mrivanplays.annotationconfig.core.serialization;
+
+/**
+ * Represents a serialization context. This holds information about the {@link
+ * java.lang.reflect.Field} that's being (de)serialized, but since AnnotationConfig can not always
+ * get a {@link java.lang.reflect.Field} instance, this context class will hold information.
+ *
+ * @param <T> generic type of information that's being (de)serialized
+ * @author MrIvanPlays
+ * @since 3.0.0
+ */
+public final class SerializationContext<T> {
+
+  // todo
+
+  /*
+  private final Optional<String> name;
+  private final T def;
+  private final Class<?> classType;
+  private final Type genericType;
+  private final Type genericSuperclass;
+   */
+
+}
