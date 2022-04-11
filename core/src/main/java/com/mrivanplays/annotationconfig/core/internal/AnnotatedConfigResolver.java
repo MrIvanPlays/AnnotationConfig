d6c2ec47b64c9658359a97140cf7cce50c6c7d3a
Ivan Pekov <ivan@mrivanplays.com>
2022-04-11 21:01:50 +0300

    Populate raw config field on dump

--- core/src/main/java/com/mrivanplays/annotationconfig/core/internal/AnnotatedConfigResolver.java
+++ core/src/main/java/com/mrivanplays/annotationconfig/core/internal/AnnotatedConfigResolver.java
@@ -152,6 +152,7 @@ public final class AnnotatedConfigResolver {
       boolean reverseFields)
       throws IOException {
     WriteData parentData = new WriteData();
+    Field rawConfigField = null;
     for (Map.Entry<AnnotationHolder, Set<AnnotationType>> entry : map.entrySet()) {
       AnnotationHolder holder = entry.getKey();
       if (holder.isClass()) {
@@ -174,6 +175,7 @@ public final class AnnotatedConfigResolver {
         }
       } else if (entry.getValue().size() == 1
           && entry.getValue().contains(AnnotationType.RAW_CONFIG)) {
+        rawConfigField = holder.getField();
         continue;
       }
       try {
@@ -186,6 +188,18 @@ public final class AnnotatedConfigResolver {
         parentData.getFieldComments().putAll(current.getFieldComments());
       } catch (IllegalAccessException e) {
         throw new IllegalArgumentException("A field became inaccessible");
+      }
+    }
+    if (rawConfigField != null) {
+      rawConfigField.setAccessible(true);
+      if (!rawConfigField.getType().isAssignableFrom(DataObject.class)) {
+        throw new IllegalArgumentException("@RawConfig on a field which is not DataObject");
+      }
+      try {
+        rawConfigField.set(annotatedConfig, new DataObject(parentData.getToWrite(), true));
+      } catch (IllegalAccessException e) {
+        throw new IllegalArgumentException(
+            "Could not set a field's value ; field not accessible anymore");
       }
     }
     valueWriter.write(parentData.getToWrite(), parentData.getFieldComments(), writer, options);
---
+++ core/src/test/java/com/mrivanplays/annotationconfig/core/TestRawConfigDump.java
@@ -0,0 +1,31 @@
+package com.mrivanplays.annotationconfig.core;
+
+import com.mrivanplays.annotationconfig.core.annotations.RawConfig;
+import com.mrivanplays.annotationconfig.core.serialization.DataObject;
+import java.io.StringWriter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestRawConfigDump {
+
+  static class Subject {
+
+    private String foo = "aabb";
+    private int bar = 1;
+
+    @RawConfig
+    public DataObject raw;
+
+  }
+
+  @Test
+  public void testRawConfigDump() {
+    Subject config = new Subject();
+    PropertyConfig.getConfigResolver().dump(config, new StringWriter());
+
+    Assertions.assertNotNull(config.raw);
+    Assertions.assertEquals("aabb", config.raw.get("foo").getAsString());
+    Assertions.assertEquals(1, config.raw.get("bar").getAsInt());
+  }
+
+}
