8acbc04b4c6c3c6f42ba9a9c8d08d63b1dcde8b8
Ivan Pekov <ivan@mrivanplays.com>
2022-04-21 10:57:58 +0300

    improve reflectionutils

--- core/src/main/java/com/mrivanplays/annotationconfig/core/utils/ReflectionUtils.java
+++ core/src/main/java/com/mrivanplays/annotationconfig/core/utils/ReflectionUtils.java
@@ -34,84 +34,7 @@ public final class ReflectionUtils {
     if (nameType == null) {
       throw new IllegalArgumentException("Not primitive");
     }
-    switch (nameType) {
-      case BYTE:
-        byte[] b = (byte[]) obj;
-        Byte[] bArr = new Byte[b.length];
-        for (int i = 0; i < b.length; i++) {
-          bArr[i] = b[i];
-        }
-        return bArr;
-      case DOUBLE:
-        double[] d = (double[]) obj;
-        Double[] dArr = new Double[d.length];
-        for (int i = 0; i < d.length; i++) {
-          dArr[i] = d[i];
-        }
-        return dArr;
-      case FLOAT:
-        float[] f = (float[]) obj;
-        Float[] fArr = new Float[f.length];
-        for (int i = 0; i < f.length; i++) {
-          fArr[i] = f[i];
-        }
-        return fArr;
-      case INTEGER:
-        int[] in = (int[]) obj;
-        Integer[] iArr = new Integer[in.length];
-        for (int i = 0; i < in.length; i++) {
-          iArr[i] = in[i];
-        }
-        return iArr;
-      case SHORT:
-        short[] s = (short[]) obj;
-        Short[] sArr = new Short[s.length];
-        for (int i = 0; i < s.length; i++) {
-          sArr[i] = s[i];
-        }
-        return sArr;
-      case LONG:
-        long[] l = (long[]) obj;
-        Long[] lArr = new Long[l.length];
-        for (int i = 0; i < l.length; i++) {
-          lArr[i] = l[i];
-        }
-        return lArr;
-      case CHAR:
-        char[] c = (char[]) obj;
-        Character[] cArr = new Character[c.length];
-        for (int i = 0; i < c.length; i++) {
-          cArr[i] = c[i];
-        }
-        return cArr;
-      case BOOLEAN:
-        boolean[] bo = (boolean[]) obj;
-        Boolean[] boArr = new Boolean[bo.length];
-        for (int i = 0; i < bo.length; i++) {
-          boArr[i] = bo[i];
-        }
-        return boArr;
-      case STRING:
-        return (String[]) obj;
-      case BIG_INTEGER:
-        // transform BigIntegers to long arrays
-        BigInteger[] bigIntegerArray = (BigInteger[]) obj;
-        Long[] bigIntRet = new Long[bigIntegerArray.length];
-        for (int i = 0; i < bigIntegerArray.length; i++) {
-          bigIntRet[i] = bigIntegerArray[i].longValueExact();
-        }
-        return bigIntRet;
-      case BIG_DECIMAL:
-        // transform BigDecimals to double arrays
-        BigDecimal[] bigDecimalArray = (BigDecimal[]) obj;
-        Double[] bigDecRet = new Double[bigDecimalArray.length];
-        for (int i = 0; i < bigDecimalArray.length; i++) {
-          bigDecRet[i] = bigDecimalArray[i].doubleValue();
-        }
-        return bigDecRet;
-      default:
-        throw new IllegalArgumentException("Something went wrong with casting Object to an array");
-    }
+    return nameType.castToArray(obj);
   }
 
   private ReflectionUtils() {
@@ -119,17 +42,124 @@ public final class ReflectionUtils {
   }
 
   private enum PrimitiveNameTypes {
-    BYTE(byte.class, "byte", "java.lang.Byte"),
-    DOUBLE(double.class, "double", "java.lang.Double"),
-    FLOAT(float.class, "float", "java.lang.Float"),
-    INTEGER(int.class, "int", "java.lang.Integer"),
-    SHORT(short.class, "short", "java.lang.Short"),
-    LONG(long.class, "long", "java.lang.Long"),
-    CHAR(char.class, "char", "java.lang.Character"),
-    BOOLEAN(boolean.class, "boolean", "java.lang.Boolean"),
-    STRING(String.class, "String", "java.lang.String"),
-    BIG_INTEGER(BigInteger.class, "BigInteger", "java.math.BigInteger"),
-    BIG_DECIMAL(BigDecimal.class, "BigDecimal", "java.math.BigDecimal");
+    BYTE(byte.class, "byte", "java.lang.Byte") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        byte[] b = (byte[]) obj;
+        Byte[] bArr = new Byte[b.length];
+        for (int i = 0; i < b.length; i++) {
+          bArr[i] = b[i];
+        }
+        return bArr;
+      }
+    },
+    DOUBLE(double.class, "double", "java.lang.Double") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        double[] d = (double[]) obj;
+        Double[] dArr = new Double[d.length];
+        for (int i = 0; i < d.length; i++) {
+          dArr[i] = d[i];
+        }
+        return dArr;
+      }
+    },
+    FLOAT(float.class, "float", "java.lang.Float") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        float[] f = (float[]) obj;
+        Float[] fArr = new Float[f.length];
+        for (int i = 0; i < f.length; i++) {
+          fArr[i] = f[i];
+        }
+        return fArr;
+      }
+    },
+    INTEGER(int.class, "int", "java.lang.Integer") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        int[] in = (int[]) obj;
+        Integer[] iArr = new Integer[in.length];
+        for (int i = 0; i < in.length; i++) {
+          iArr[i] = in[i];
+        }
+        return iArr;
+      }
+    },
+    SHORT(short.class, "short", "java.lang.Short") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        short[] s = (short[]) obj;
+        Short[] sArr = new Short[s.length];
+        for (int i = 0; i < s.length; i++) {
+          sArr[i] = s[i];
+        }
+        return sArr;
+      }
+    },
+    LONG(long.class, "long", "java.lang.Long") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        long[] l = (long[]) obj;
+        Long[] lArr = new Long[l.length];
+        for (int i = 0; i < l.length; i++) {
+          lArr[i] = l[i];
+        }
+        return lArr;
+      }
+    },
+    CHAR(char.class, "char", "java.lang.Character") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        char[] c = (char[]) obj;
+        Character[] cArr = new Character[c.length];
+        for (int i = 0; i < c.length; i++) {
+          cArr[i] = c[i];
+        }
+        return cArr;
+      }
+    },
+    BOOLEAN(boolean.class, "boolean", "java.lang.Boolean") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        boolean[] bo = (boolean[]) obj;
+        Boolean[] boArr = new Boolean[bo.length];
+        for (int i = 0; i < bo.length; i++) {
+          boArr[i] = bo[i];
+        }
+        return boArr;
+      }
+    },
+    STRING(String.class, "String", "java.lang.String") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        return (String[]) obj;
+      }
+    },
+    BIG_INTEGER(BigInteger.class, "BigInteger", "java.math.BigInteger") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        // transform BigIntegers to long arrays
+        BigInteger[] bigIntegerArray = (BigInteger[]) obj;
+        Long[] bigIntRet = new Long[bigIntegerArray.length];
+        for (int i = 0; i < bigIntegerArray.length; i++) {
+          bigIntRet[i] = bigIntegerArray[i].longValueExact();
+        }
+        return bigIntRet;
+      }
+    },
+    BIG_DECIMAL(BigDecimal.class, "BigDecimal", "java.math.BigDecimal") {
+      @Override
+      public Object[] castToArray(Object obj) {
+        // transform BigDecimals to double arrays
+        BigDecimal[] bigDecimalArray = (BigDecimal[]) obj;
+        Double[] bigDecRet = new Double[bigDecimalArray.length];
+        for (int i = 0; i < bigDecimalArray.length; i++) {
+          bigDecRet[i] = bigDecimalArray[i].doubleValue();
+        }
+        return bigDecRet;
+      }
+    };
 
     public static PrimitiveNameTypes getNameType(String typeName) {
       for (PrimitiveNameTypes val : PrimitiveNameTypes.values()) {
@@ -149,5 +179,7 @@ public final class ReflectionUtils {
       this.primitiveType = primitiveType;
       this.names = names;
     }
+
+    public abstract Object[] castToArray(Object array);
   }
 }
